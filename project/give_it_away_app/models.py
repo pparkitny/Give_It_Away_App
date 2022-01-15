9e49f486aee7eb42c8774bc20822db7e7f0da521
Patryk Parkitny <pat.parkitny@gmail.com>
2022-01-15 15:19:48 +0100

    Add Institution model

--- project/give_it_away_app/models.py
+++ project/give_it_away_app/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 
+TYPES_OF_INSTITUTION = (
+    (1, "Fundacja"),
+    (2, "Organizacja pozarządowa"),
+    (3, "Zbiórka lokalna")
+)
+
 
 class Category(models.Model):
     name = models.CharField(max_length=64)
 
     def __str__(self):
         return self.name
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=128)
+    type = models.IntegerField(choices=TYPES_OF_INSTITUTION)
+    categories = models.ManyToManyField(Category)
