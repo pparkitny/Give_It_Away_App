97334d53775e98a3da791a4e6c83766ff01bd484
Patryk Parkitny <pat.parkitny@gmail.com>
2022-01-15 19:45:31 +0100

    Add count on main site

--- project/give_it_away_app/views.py
+++ project/give_it_away_app/views.py
@@ -2,11 +2,37 @@ from django.shortcuts import render
 from django.views import View
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from .models import Category, Institution
+from .models import Category, Institution, Donation
 
 
 class LandingPageView(View):
     def get(self, request):
+        number_of_donations = Donation.objects.count()
+        sum_of_bags = 0
+        for i in range(number_of_donations + 1): # count number of bags
+            if i == 0:
+                pass
+            else:
+                obj = Donation.objects.get(id=i)
+                sum_of_bags += obj.quantity
+
+        list_of_donated_institutions = []
+        for i in range(number_of_donations + 1):
+            if i == 0:
+                pass
+            else:
+                obj = Donation.objects.get(id=i)
+                if obj.institution in list_of_donated_institutions:
+                    pass
+                else:
+                    list_of_donated_institutions.append(obj.institution)
+                number_of_donated_institutions = len(list_of_donated_institutions)
+
+        ctx = {'sum_of_bags': sum_of_bags, 'number_of_donated_institutions': number_of_donated_institutions}
+
+        return render(request, 'index.html', ctx)
+
+    def post(self, request):
         return render(request, 'index.html')
 
 
