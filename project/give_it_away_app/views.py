dd43f1373fd61bb692340956337057bde51f75a3
Patryk Parkitny <pat.parkitny@gmail.com>
2022-01-15 14:53:40 +0100

    Add first simple views, modify html files, add urls

--- project/give_it_away_app/views.py
+++ project/give_it_away_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views import View
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+
+class LandingPageView(View):
+    def get(self, request):
+        return render(request, 'index.html')
+
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, 'form.html')
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, 'register.html')
+
